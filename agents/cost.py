c076235435b731fc7834c48caff6b677490f3e51
Cédric JUNG <cedced19@gmail.com>
2022-03-31 11:33:47 +0200

    Add tabou object

---
+++ agents/cost.py
@@ -0,0 +1,12 @@
+
+
+def cout(solution, matrice, w):
+    K = len(solution) - solution.count([])
+    sum_cost = 0
+    for route in solution:
+        if (len(route)):
+            sum_cost += matrice[0, route[0]] # ajouter 0->premier el
+            sum_cost += matrice[0, route[-1]] # ajouter dernier el->0
+            for i in range(len(route)-1):
+                sum_cost += matrice[route[i], route[i+1]]
+    return K*w + sum_cost
